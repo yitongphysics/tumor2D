f666b5ebf9ee565c3890a64b211bc981cd4bc64e
yitongphysics <69072435+yitongphysics@users.noreply.github.com>
2023-03-25 14:12:04 -0400

    check

--- postCode/compute_ISF_func.m
+++ postCode/compute_ISF_func.m
@@ -1,6 +1,8 @@
-clear;clc;close all;
+function compute_ISF(sim_name)
 
-fstr = '/Users/yitongzheng/Documents/Corey/tumor2D/OverDamped/0221_P0/0.0002.pos';
+sim_name = string(sim_name*0.0002);
+pathname = append(pwd,'/');
+fstr = append(pathname,sim_name,'.pos');
 
 finfo = dir(fstr);
 fprintf('-- Reading in %s\n',finfo.name);
@@ -19,7 +21,7 @@ X=zeros(1000,1000);
 Y=zeros(1000,1000);
 
 for ii = FSTART:FEND
-    fprintf('printing frame ii = %d/%d\n',ii,FEND);
+
     % read in data
     tumorConfigData = readTumor2DInterface(fid);
     if tumorConfigData.NCELLS==0
@@ -31,13 +33,13 @@ for ii = FSTART:FEND
     
     x = tumorConfigData.x;
     y = tumorConfigData.y;
+    %fprintf('reading frame ii = %d/%d\n',ii,FEND);
     
     X(ii,:) = [x{1:1000}];
     Y(ii,:) = [y{1:1000}];
 end
 X(ii+1:end,:) = [];
 Y(ii+1:end,:) = [];
-        
 Ly=7.9330033363271;
 for jj=1:ii-1
     Y(jj+1:end,:) = Y(jj+1:end,:)-(Y(jj+1,:)-Y(jj,:)>Ly/2)*Ly;
@@ -47,23 +49,24 @@ for jj=1:ii
     Y(jj,:) = Y(jj,:) - mean(Y(jj,:));
 end
 %%
-close all
 Nframe = length([X(:,1)]);
 dt=[0:Nframe-1];
 isf = zeros(1,Nframe);
-Ly=7.9330033363271;
 k=200;
 for tt=0:Nframe-1
     X1 = X(1:Nframe-tt,:);X1 = reshape(X1,1,[]);
     X2 = X(1+tt:end,:);X2 = reshape(X2,1,[]);
     Y1 = Y(1:Nframe-tt,:);Y1 = reshape(Y1,1,[]);
     Y2 = Y(1+tt:end,:);Y2 = reshape(Y2,1,[]);
-    %X1 = X(tt,:);X1 = reshape(X1,1,[]);
-    %X2 = X(1,:);X2 = reshape(X2,1,[]);
-    %Y1 = Y(tt,:);Y1 = reshape(Y1,1,[]);
-    %Y2 = Y(1,:);Y2 = reshape(Y2,1,[]);
     isf(tt+1) = compute_ISF_2D(X1, Y1, X2, Y2, k, Ly);
-    tt
 end
-plot(isf)
-set(gca, 'XScale', 'log')
+
+%%
+yourFolder = append(pathname,'ISF');
+if ~exist(yourFolder, 'dir')
+   mkdir(yourFolder)
+end
+data_name = append(pathname,'ISF/',sim_name,'_ISF.mat');
+save(data_name,'dt','isf');
+
+end
