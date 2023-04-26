ea5fa679980b3b59d113656546a9432bc3100fb4
yitongphysics <69072435+yitongphysics@users.noreply.github.com>
2023-04-26 00:13:47 -0400

    No bug version, much faster.

--- dpm.h
+++ dpm.h
@@ -1,20 +1,9 @@
 #ifndef DPM_H
 #define DPM_H
-
-/*
-
-	HEADER FILE FOR DPM CLASS
-
-		-- Stores vertex information for NCELLS with n_\mu vertices each
-		-- Stores shape parameters + spring constants
-		-- Integrates equations of motion (NVE/NVT)
-		-- Finds specified configurations (jammed, fixed p, fixed shear strain, etc)
-		-- Compute mechanical information (Hessian, elastic moduli, etc)
-		-- Prints data to files
-
-	Jack Treado, 04/10/21
-
-*/
+#define SNUM         42 // total number of sites/vertices on one cell
+#define FNUM         80 // total number of faces on one cell
+#define ENUM         120 // total number of edges on one cell
+#define NN_NUM         741
 
 
 #include <iostream>
@@ -25,9 +14,9 @@
 #include <cmath>
 #include <vector>
 #include <functional>
-#include <Eigen/Core>
-#include <Eigen/Dense>
-#include <Eigen/Eigenvalues>
+#include <algorithm>
+#include <random>
+
 
 // pointer-to-member function call macro
 #define CALL_MEMBER_FN(object, ptrToMember) ((object).*(ptrToMember))
@@ -45,7 +34,7 @@ const int wnum = 25;
 const int pnum = 14;
 
 // FIRE constants
-const double alpha0 = 0.2;
+const double alpha0 = 0.15;
 const double finc = 1.1;
 const double fdec = 0.5;
 const double falpha = 0.99;
@@ -71,10 +60,11 @@ protected:
 
 	// potential energy
 	double U;
-
+    double Udpm;
+    
 	// particle spring constants
+	double kv;
 	double ka;
-	double kl;
 	double kb;
 	double kc;
 
@@ -86,27 +76,26 @@ protected:
 	std::vector<bool> pbc;
 
 	// particle shape parameters
-	std::vector<double> a0;
-	std::vector<double> l0;
-	std::vector<double> t0;
 	std::vector<double> r;
-
+    double D0_inter_unit;
+    
 	// indexing variables
 	std::vector<int> nv;
+    std::vector<double> V0;
 	std::vector<int> szList;
-	std::vector<int> im1;
-	std::vector<int> ip1;
+    std::vector<int> C_list;
+    
+    std::vector<std::vector<int>>        edgelist;
+    std::vector<std::vector<int>>        f_unit;
+    std::vector<std::vector<int>>        nnlist;
+    std::vector<double>             A0_unit;
+    std::vector<double>             A0;
+    std::vector<double>             xyz_unit;
 
 	// dynamical variables
 	std::vector<double> x;
 	std::vector<double> v;
 	std::vector<double> F;
-
-	// macroscopic stress vector
-	std::vector<double> stress;
-
-	// contact network
-	std::vector<int> cij;
 
 	// Box linked-list variables
 	int NBX;
@@ -124,64 +113,26 @@ public:
 	// Constructors and Destructors
 	dpm(int ndim);
 	dpm(int n, int ndim, int seed);
-	dpm(int n, int seed) : dpm(n, 2, seed) {}
+	dpm(int n, int seed) : dpm(n, 3, seed) {}
 	~dpm();
 
 	// -- G E T T E R S
-
-	// main ints
-	int getNCELLS() { return NCELLS; };
-	int getNDIM() { return NDIM; };
-	int getNNN() { return NNN; };
-	int getNVTOT() { return NVTOT; };
-	int getvertDOF() { return vertDOF; };
-	int getNV(int ci) { return nv.at(ci); };
-
-	// force parameters
-	double getdt() { return dt; };
-	double getka() { return ka; };
-	double getkl() { return kl; };
-	double getkb() { return kb; };
-	double getkc() { return kc; };
-
-	// static cell info
-	double geta0(int ci) { return a0[ci]; };
-	double getl0(int gi) { return l0[gi]; };
-	double gett0(int gi) { return t0[gi]; };
-	double getr(int gi) { return r[gi]; };
-
-	// dynamic cell info
-	double getx(int gi, int d) { return x[NDIM * gi + d]; };
-	double getv(int gi, int d) { return v[NDIM * gi + d]; };
-	double getF(int gi, int d) { return F[NDIM * gi + d]; };
-	double getU() { return U; };
-
-	// get stress info
-	double getstress(int d) { return stress.at(d); };
-
-	// boundary variables
-	double getL(int d) { return L.at(d); };
-	bool getpbc(int d) { return pbc.at(d); };
-
 	// cell shape indexing + information
 	int gindex(int ci, int vi);
 	void cindices(int &ci, int &vi, int gi);
+	double volume(int ci);
 	double area(int ci);
-	double area(int ci, double gamma);
-	double perimeter(int ci);
-	void com2D(int ci, double &cx, double &cy);
-	double vertexPackingFraction2D();
-	double vertexPreferredPackingFraction2D();
-	double vertexKineticEnergy();
-	int vvContacts();
-	int ccContacts();
+	void com3D(int ci, double &cx, double &cy, double &cz);
+    void cof3D(int ci, double &fx, double &fy, double &fz);
+    void cov3D(int ci, double &vx, double &vy, double &vz);
+    double NN3D(int gi);
+	double vertexPackingFraction3D();
+	double vertexPreferredPackingFraction3D();
 
 	// Setters
-	void setpbc(int d, bool val) { pbc.at(d) = val; };
-	void setNCELLS(int val) { NCELLS = val; };
 	void setdt(double val);
-	void setka(double val) { ka = val; };
-	void setkl(double val) { kl = val; };
+    void setka(double val) { ka = val; };
+	void setkv(double val) { kv = val; };
 	void setkb(double val) { kb = val; };
 	void setkc(double val) { kc = val; };
 	void setl1(double val) { l1 = val; };
@@ -199,59 +150,12 @@ public:
 			std::cout << "** Opening pos file " << str << " ..." << std::endl;
 	}
     
-	// Initialize particles (two dimensions)
-	void monodisperse2D(int n);
-	void bidisperse2D(double calA0, int nsmall, double smallfrac, double sizefrac);
-	void gaussian2D(double dispersion, double calA0, int n1);
-	void sinusoidalPreferredAngle(double thA, double thK);
-	void initializeVertexShapeParameters(double calA0, int nref);
+	// Initialize particles (three dimensions)
 	void initializeVertexShapeParameters(int ci, double calA0, double lenscale);
-	void initializeVertexIndexing2D();
-	void initializePositions2D(double phi0, double Ftol);
-	void initializeNeighborLinkedList2D(double boxLengthScale);
+	void initializeNeighborLinkedList3D(double boxLengthScale);
 
 	// editing & updating
-	void sortNeighborLinkedList2D();
-	void scaleParticleSizes2D(double scaleFactor);
-	int removeRattlers();
-	void drawVelocities2D(double T);
-
-	// force definitions
-	void resetForcesAndEnergy();
-	void shapeForces2D();
-	void vertexRepulsiveForces2D();
-	void vertexRepulsiveForces2D(double gamma);
-	void vertexAttractiveForces2D();
-
-	// force updates
-	void repulsiveForceUpdate();
-	void attractiveForceUpdate();
-
-	// simple integrators
-	void vertexFIRE2D(dpmMemFn forceCall, double Ftol, double dt0);
-	void vertexNVE2D(std::ofstream &enout, dpmMemFn forceCall, double T, double dt0, int NT, int NPRINTSKIP);
-	void vertexLangevinNVT2D(std::ofstream &enout, dpmMemFn forceCall, double T0, double gam, double dt0, int NT, int NPRINTSKIP);
-	void vertexLangevinNVT2D(dpmMemFn forceCall, double T0, double gam, double dt0, int NT, int NPRINTSKIP);
-
-	// protocols
-	void vertexCompress2Target2D(dpmMemFn forceCall, double Ftol, double dt0, double phi0Target, double dphi0);
-	void vertexJamming2D(dpmMemFn forceCall, double Ftol, double Ptol, double dt0, double dphi0, bool plotCompression);
-	void vertexAnneal2Jam2D(dpmMemFn forceCall, double Ftol, double Ptol, double dt0, double dphi0, double T0, double trun, bool plotCompression);
-	void vertexEnthalpyMin(dpmMemFn forceCall, double Ftol, double dPtol, double P0, double dt0, bool plotCompression);
-
-	// hessian methods
-	// note: dynamical matrix contribution is always M = H - S
-	void dpmHessian2D(Eigen::MatrixXd &H, Eigen::MatrixXd &S);
-	void dpmAreaHessian2D(Eigen::MatrixXd &Ha, Eigen::MatrixXd &Sa);
-	void dpmPerimeterHessian2D(Eigen::MatrixXd &Hl, Eigen::MatrixXd &Sl);
-	void dpmBendingHessian2D(Eigen::MatrixXd &Hb, Eigen::MatrixXd &Sb);
-	void dpmRepulsiveHarmonicSprings2D(Eigen::MatrixXd &Hvv, Eigen::MatrixXd &Svv);
-
-	// print vertex information to file
-	void printNeighborList();
-	void printContactMatrix();
-	void printConfiguration2D();
-	void printHessianEigenvalues2D(std::ofstream &hessout, Eigen::MatrixXd &M);
+	void scaleParticleSizes3D(double scaleFactor);
 };
 
 #endif
--- interfaceCompression.cpp
+++ interfaceCompression.cpp
@@ -1,53 +1,68 @@
 // header files
-#include "tumor2D.h"
+/*
+#include "tumor3D.h"
 #include <sstream>
 #include <numeric>
 // preprocessor macros
-#define NDIM 2
+#define NDIM 3
 
 // namspace
 using namespace std;
 
 // global constants
 const double dphi0= 0.01;	   		// packing fraction increment during initial growth step
-const double boxLengthScale = 1.0; 	// neighbor list box size in units of initial l0
-const double phi0 = 0.6;		   	// initial packing fraction
-const double dt0 = 5e-2;		   	// initial magnitude of time step in units of MD time
-const double Ftol = 1e-8;			// force tolerance during energy min
-
-
-// mechanical constants
-//const double ka = 1.0;
-//const double kl = 0.001;
-//const double kb = 0.0;
-//const double kc = 0.1;
+const double boxLengthScale = 1.5; 	// neighbor list box size
+const double phi0 = 0.01;		   	// initial packing fraction
+const double dt0 = 0.03;		   	// initial magnitude of time step in units of MD time
+const double Ftol = 1e-7;			// force tolerance during energy min
 
 int main(int argc, char const *argv[])
 {
 	// local variables to be read in
 	int NCELLS, aN, tN, aNV, tNV, seed;
-	double aDisp, tDisp, aCalA0, tCalA0, areaRatio, prt, ka, kl, kb, kc, P0, aspectRatio;
+	double aDisp, tDisp, aCalA0, tCalA0, volumeRatio, prt, kv, ka, kb, kc, P0, aspectRatio;
 
 	// read in parameters from command line input
-	string aN_str 			= argv[1];
-	string aNV_str 			= argv[2];
-	string tNV_str 			= argv[3];
-	string aDisp_str 		= argv[4];
-	string tDisp_str 		= argv[5];
-	string aCalA0_str 		= argv[6];
-	string tCalA0_str 		= argv[7];
-	string areaRatio_str 	= argv[8];
+    
+	string aN_str 			= "28";
+	string aNV_str 			= "42";
+	string tNV_str 			= "1";
+	string aDisp_str 		= "0.0";
+	string tDisp_str 		= "0.0";
+	string aCalA0_str 		= "1.1";//1.02405832074679303822506426513428
+	string tCalA0_str 		= "1.0";
+	string volumeRatio_str 	= "64.0";
+    string aspectRatio_str  = "1.0";
+	string prt_str 			= "0.2";
+    string kv_str           = "0.01";             // ka
+    string ka_str           = "0.01";             // kl
+    string kc_str           = "0.03";             // kc
+    string kb_str           = "0.0";             // kb
+    string P0_str           = "0.0001";
+	string seed_str 		= "1";
+	string positionFile 	= "/Users/yitongzheng/Documents/Corey/tumor3D/C.test";
+    
+    /*
+    string aN_str             = argv[1];
+    string aNV_str             = argv[2];
+    string tNV_str             = argv[3];
+    string aDisp_str         = argv[4];
+    string tDisp_str         = argv[5];
+    string aCalA0_str         = argv[6];
+    string tCalA0_str         = argv[7];
+    string volumeRatio_str     = argv[8];
     string aspectRatio_str  = argv[9];
-	string prt_str 			= argv[10];
-    string ka_str           = argv[11];             // ka
-    string kl_str           = argv[12];             // kl
+    string prt_str             = argv[10];
+    string kv_str           = argv[11];             // ka
+    string ka_str           = argv[12];             // kl
     string kc_str           = argv[13];             // kc
     string kb_str           = argv[14];             // kb
     string P0_str           = argv[15];
-	string seed_str 		= argv[16];
-	string positionFile 	= argv[17];
-
-	// using sstreams to get parameters
+    string seed_str         = argv[16];
+    string positionFile     = argv[17];
+    */
+	// using sstreams to get p
+/*arameters
 	stringstream aNss(aN_str);
 	stringstream aNVss(aNV_str);
 	stringstream tNVss(tNV_str);
@@ -55,11 +70,11 @@ int main(int argc, char const *argv[])
 	stringstream tDispss(tDisp_str);
 	stringstream aCalA0ss(aCalA0_str);
 	stringstream tCalA0ss(tCalA0_str);
-	stringstream areaRatioss(areaRatio_str);
+	stringstream volumeRatioss(volumeRatio_str);
     stringstream aspectRatioss(aspectRatio_str);
 	stringstream prtss(prt_str);
+    stringstream kvss(kv_str);
     stringstream kass(ka_str);
-    stringstream klss(kl_str);
     stringstream kcss(kc_str);
     stringstream kbss(kb_str);
     stringstream P0ss(P0_str);
@@ -73,51 +88,202 @@ int main(int argc, char const *argv[])
 	tDispss 		>> tDisp;
 	aCalA0ss 		>> aCalA0;
 	tCalA0ss 		>> tCalA0;
-	areaRatioss 	>> areaRatio;
+	volumeRatioss 	>> volumeRatio;
     aspectRatioss   >> aspectRatio;
 	prtss 			>> prt;
+    kvss            >> kv;
     kass            >> ka;
-    klss            >> kl;
     kcss            >> kc;
     kbss            >> kb;
     P0ss            >> P0;
 	seedss 			>> seed;
     
-	// determine number of tumor cells based on areaRatio and prt
-	//tN = round(aN * areaRatio * (prt/(1.0 - prt)));
-    tN = 1000;
+	// determine number of tumor cells based on volumeRatio and prt
+	//tN = round(aN * volumeRatio * (prt/(1.0 - prt)));
+    tN = 1500;
 	NCELLS = tN + aN;
 
+    cout.precision(10);
 	// instantiate object
-	tumor2D tumor2Dobj(NCELLS, tN, seed);
-
+	tumor3D tumor3Dobj(NCELLS, tN, seed);
+    
 	// open position config file
-	tumor2Dobj.openPosObject(positionFile);
+	tumor3Dobj.openPosObject(positionFile);
 
 	// set spring constants
-	tumor2Dobj.setka(ka);
-	tumor2Dobj.setkl(kl);
-	tumor2Dobj.setkb(kb);
-	tumor2Dobj.setkc(kc);
+	tumor3Dobj.setkv(kv);
+	tumor3Dobj.setka(ka);
+	tumor3Dobj.setkb(kb);
+	tumor3Dobj.setkc(kc);
 
+    tumor3Dobj.readPolyhedron();
+    
 	// initialize adipocyte and tumor cells
-	tumor2Dobj.initializeTumorInterface(aCalA0, tCalA0, aDisp, tDisp, areaRatio, aNV, tNV);
+	tumor3Dobj.initializeTumorInterface(aCalA0, volumeRatio, aNV, tNV);
 
 	// initialize particle positions
-	tumor2Dobj.initializeTumorInterfacePositions(phi0, Ftol, prt, aspectRatio);
+	tumor3Dobj.initializeTumorInterfacePositions(phi0, Ftol, prt, aspectRatio);
 
 	// initialize neighbor linked list
-	tumor2Dobj.initializeNeighborLinkedList2D(boxLengthScale);
+	tumor3Dobj.initializeNeighborLinkedList3D(boxLengthScale);
 
 	// -- compression to initial condition
-	tumor2Dobj.tumorCompression(Ftol,P0,dt0,dphi0);
+	tumor3Dobj.tumorCompression(Ftol,P0,dt0,dphi0);
     
 	// print interface
-	tumor2Dobj.printTumorInterface(0.0);
+	tumor3Dobj.printTumorInterface(0.0);
 
 
 	// end
 	cout << "interfaceCompression.cpp completed, interface printed to " << positionFile << ", ending. " << endl;
 	return 0;
 }
+ 
+ 
+ 
+ 
+ 
+ // initialize neighbor linked list
+ void dpm::initializeNeighborLinkedList3D(double boxLengthScale) {
+     // local variables
+     double llscale;
+     int i, d, scx, scy, scz, boxid;
+     int ci, gi, vi;
+     
+     // print to console
+     //cout << "** initializing neighbor linked list, boxLengthScale = " << boxLengthScale;
 
+     // get largest radius as llscale
+     llscale = 2.0 * (*max_element(r.begin(),r.end()));
+
+     // initialize box length vectors
+     NBX = 1;
+     sb.resize(NDIM);
+     lb.resize(NDIM);
+     for (d = 0; d < NDIM; d++) {
+         // determine number of cells along given dimension by rmax
+         sb[d] = floor(L[d] / (boxLengthScale * llscale));
+
+         // just in case, if < 3, change to 3 so box neighbor checking will work
+         if (sb[d] < 3)
+             sb[d] = 3;
+
+         // determine box length by number of cells
+         lb[d] = L[d] / sb[d];
+
+         // count total number of cells
+         NBX *= sb[d];
+     }
+
+     // initialize list of box nearest neighbors
+     scx = sb[0];
+     scy = sb[1];
+     scz = sb[2];
+     NBX = scx * scy * scz;
+     NNN = 13; // number of nearest neighbors for each cell in 3D
+
+     nn.resize(NBX);
+
+     // loop over cells, save forward neighbors for each box
+     for (i = 0; i < NBX; i++) {
+         // reshape entry
+         nn[i].resize(NNN);
+
+         int ix = i % scx;
+         int iy = (i / scx) % scy;
+         int iz = i / (scx * scy);
+
+         // Index for nn[i]
+         int nn_idx = 0;
+
+         for (int dz = 0; dz <= 1; ++dz) {
+             for (int dy = -1; dy <= 1; ++dy) {
+                 for (int dx = -1; dx <= 1; ++dx) {
+                     // Skip the cases that lead to double counting
+                     if (dz == 0 && dy == 0 && dx <= 0) continue;
+                     if (dz == 0 && dy == -1) continue;
+                     if (dz == 0 && dy == 0 && dx == -1) continue;
+                     
+                     int nx = ix + dx;
+                     int ny = iy + dy;
+                     int nz = iz + dz;
+
+                     // Apply periodic boundary conditions if enabled
+                     if (pbc[0]) nx = (nx + scx) % scx;
+                     if (pbc[1]) ny = (ny + scy) % scy;
+                     if (pbc[2]) nz = (nz + scz) % scz;
+
+                     // Check if the neighbor cell is within the simulation box (non-PBC case)
+                     bool inside_box = (nx >= 0 && nx < scx) && (ny >= 0 && ny < scy) && (nz >= 0 && nz < scz);
+
+                     // Calculate linear neighbor index and store it in the nn vector
+                     int neighbor_idx = inside_box ? (nx + ny * scx + nz * scx * scy) : -1;
+                     nn[i][nn_idx] = neighbor_idx;
+
+                     ++nn_idx;
+                 }
+             }
+         }
+     }
+
+
+     // linked-list variables
+     head.resize(NBX);
+     last.resize(NBX);
+     list.resize(NVTOT + 1);
+     C_list.resize(NVTOT);
+
+     // print box info to console
+     //cout << ";  initially NBX = " << NBX << " ..." << endl;
+
+     fill(list.begin(), list.end(), 0);
+     fill(head.begin(), head.end(), 0);
+     fill(last.begin(), last.end(), 0);
+     fill(C_list.begin(), C_list.end(), 0);
+     
+     for (gi = 0; gi < NVTOT; gi++) {
+         int ix = int(x[NDIM * gi] / L[0] * scx);
+         int iy = int(x[NDIM * gi+1] / L[1] * scy);
+         int iz = int(x[NDIM * gi+2] / L[2] * scz);
+
+         // Ensure the particle is within the simulation box bounds
+         if (ix >= scx) ix = scx - 1;
+         if (iy >= scy) iy = scy - 1;
+         if (iz >= scz) iz = scz - 1;
+         if (ix < 0) ix = 0;
+         if (iy < 0) iy = 0;
+         if (iz < 0) iz = 0;
+
+         boxid = ix + iy * scx + iz * scx * scy;
+         if (head[boxid] == 0) {
+             head[boxid] = gi + 1;
+             last[boxid] = gi + 1;
+         }
+         else {
+             list[last[boxid]] = gi + 1;
+             last[boxid] = gi + 1;
+         }
+         
+         //initialize C_list
+         cindices(ci,vi, gi);
+         C_list[gi] = ci;
+     }
+     /*
+     NBX=1;
+     fill(list.begin(), list.end(), 0);
+     fill(head.begin(), head.end(), 0);
+     fill(last.begin(), last.end(), 0);
+     for (int gi = 0; gi < NVTOT; gi++) {
+         if (head[0] == 0) {
+             head[0] = gi + 1;
+             last[0] = gi + 1;
+         }
+         else {
+             list[last[0]] = gi + 1;
+             last[0] = gi + 1;
+         }
+     }
+     list[0] = 1;
+      *//*
+ }
+*/
